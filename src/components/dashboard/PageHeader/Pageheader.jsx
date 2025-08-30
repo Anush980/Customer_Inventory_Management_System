ddf28a63b8fc463d24e736cfd7da821e4212f5f0
Anush Shrestha <anush.stha980@outlook.com>
2025-08-30 19:18:33 +0545

    Add: navigation

--- src/components/dashboard/PageHeader/Pageheader.jsx
+++ src/components/dashboard/PageHeader/Pageheader.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
 import './pageHeader.css';
 
 const Pageheader = ({title,btnTitle}) => {
+   const navigate = useNavigate(); 
+
+  const handleButtonClick = () => {
+    navigate("/sales"); 
+  };
   return (
     <div className="page-header">
             <h2>Dashboard </h2>
-            <button className="btn">
+            <button className="btn" onClick={handleButtonClick}>
               <FontAwesomeIcon icon="plus" />
               <span> New Sales</span>
             </button>
