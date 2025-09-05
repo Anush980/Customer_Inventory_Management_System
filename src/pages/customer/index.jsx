c4d6350e19fd1113264169188aaa1ce7479434f9
Anush Shrestha <anush.stha980@outlook.com>
2025-09-05 19:55:18 +0545

    feat:Add responsive customer details card

--- src/pages/customer/index.jsx
+++ src/pages/customer/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import Layout from '../../components/ui/Layout/Layout'
+import Layout from '../../components/ui/Layout/Layout';
+import StatsCard from '../../components/dashboard/StatsCard/StatsCard'
+import Pageheader from '../../components/dashboard/PageHeader/Pageheader';
+import FilterBar from "../../components/dashboard/Filterbar/FilterBar"; 
+import CustomerCard from '../../components/customer/CustomerCard/CustomerCard';
+import './customer.css';
 
 const CustomerPage = () => {
   return (
-    <Layout/>
+      <Layout>
+        <Pageheader title="Customer Management" btnTitle="Add Customer"/>
+      <StatsCard value="100" change='1' type="customer"/>
+      <FilterBar/>
+      <div className="customer-cards">
+<CustomerCard/>
+<CustomerCard/>
+<CustomerCard/>
+      </div>
+      </Layout>
   )
 }
 
