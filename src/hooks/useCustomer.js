43e7ba10cd1f6704d3097f2cbc3a7f5dc10b877f
Anush Shrestha <anush.stha980@outlook.com>
2025-11-13 20:20:45 +0545

    feat:add api hooks

---
+++ src/hooks/useCustomer.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { getCustomers ,saveCustomer, deleteCustomer } from "../api/customerApi";
+
+export const useCustomers = ({search="",sort="newest"}={})=>{
+    const [customers,setCustomers]=useState([]);
+    const [loading,setLoading]= useState(false);
+    const [error, setError]= useState(null);
+
+    //Fetch Customer
+    const fetchCustomers =async()=>{
+        setLoading(true);
+        setError(null);
+        try{
+            const data = await getCustomers({search,sort});
+            setCustomers(data);
+        }
+        catch(err){
+            setError(err.message || "Failed to fetch customers.");
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+
+    //Create or update Customer
+    const saveCustomerByID= async (customer)=>{
+        try{
+            const saved = await saveCustomer(customer);
+
+            //If exists, udpate
+            if(customer._id){
+                setCustomers(prev=>
+                    prev.map(c=>(c._id === saved._id ? saved : c))
+                );
+            }
+            else{
+                //If new ,create
+                setCustomers(prev=> [saved,...prev]);
+            }
+            return saved;
+        }
+        catch(err){
+            setError(err.message || "Failed to save customer");
+            throw err;
+        }
+    };
+
+    //Delete customer
+const deleteCustomerById = async(id)=>{
+    try{
+        await deleteCustomer(id);
+        setCustomers(prev=>prev.filter(c=>c._id !==id));
+    }
+    catch(err){
+        setError(err.message || "Failed to delete customer.")
+    }
+};
+
+useEffect(()=>{
+    fetchCustomers();
+},[search,sort]);
+
+return {
+    customers,loading,error,fetchCustomers,deleteCustomerById,saveCustomerByID
+};
+
+}
