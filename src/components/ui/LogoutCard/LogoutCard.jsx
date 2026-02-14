f51a4699473e800c6a7cfdf2f18d88e913646e9c
Anush Shrestha <anush.stha980@outlook.com>
2026-02-15 03:53:33 +0545

    minor fixes

--- src/components/ui/LogoutCard/LogoutCard.jsx
+++ src/components/ui/LogoutCard/LogoutCard.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
+import { createPortal } from "react-dom";
 import Button from "../Button/Button";
 import "./logoutCard.css";
 
-const LogoutCard = ({closeWindow,onConfirm}) => {
-  const [loading,setLoading]=useState(false);
+const LogoutCard = ({ closeWindow, onConfirm }) => {
+  const [loading, setLoading] = useState(false);
 
-  return (
+  return createPortal(
     <div className="confirm-card-wrapper">
       <div className="confirm-card-content">
         <div className="confirm-card-header">
-          <h4>Log out Confirmation</h4>{" "}
+          <h4>Log out Confirmation</h4>
         </div>
         <div className="confirm-card-text">
           <span>Are you sure you want to logout?</span>
@@ -19,12 +20,20 @@ const LogoutCard = ({closeWindow,onConfirm}) => {
           <Button variant="text" onClick={closeWindow}>
             Cancel
           </Button>
-          <Button variant="danger" onClick={onConfirm} isLoading={loading}>
+          <Button
+            variant="danger"
+            onClick={() => {
+              setLoading(true);
+              onConfirm();
+            }}
+            isLoading={loading}
+          >
             Confirm
           </Button>
         </div>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 };
 
